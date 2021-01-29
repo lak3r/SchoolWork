59fc2de794a98aa0bd553ee334ae3a379f2fb8d4
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-01-29 03:14:35 -0600

    not shit

--- Project1/helper.cpp
+++ Project1/helper.cpp
@@ -7,18 +7,31 @@
 using namespace std;
 
 
-void helper::test(string t){
-	
+void helper::test(string t){	
 	cout<<t;
-	
 }
 
 void helper::readData(ifstream& input){
 	string holder;
+	dataPoint *head;
 	
-	for(int i=0; i<4; i++){
-	
+	for(int i=0; i<5; i++){
 		getline(input, holder);
 		cout<< holder << "\n";
 	}
+	
+	for(int i=0; input; i++){
+		//head = new dataPoint
+		
+		getline(input, holder);
+		if(i<10 or i%5==0) cout<<holder<<"\n";
+	}
+
+}
+
+void helper::clearData(dataPoint *head){
+	while(head.next != NULL){
+		
+	}
+	
 }
--- Project1/helper.h
+++ Project1/helper.h
@@ -5,12 +5,15 @@ struct dataPoint {
 	std::string unitsX;
 	double valueY;
 	std::string unitsY;
+	dataPoint *previous = NULL;
+	dataPoint *next = NULL;
 };
 
 class helper{
 	public:
 		void test(std::string t);
 		void readData(std::ifstream& input);
+		void clearData(dataPoint *head);
 
 };
 
