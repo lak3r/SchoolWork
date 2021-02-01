69fc10533d751fa9be5b4ff87282ab6d38ccde59
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-02-01 17:50:19 -0600

    moved things
    I'm going to have to rename this again lol

---
+++ csci6050/p1/p1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	//base test case
+	if(argc>0 and (string(argv[1]) == "-hw")){
+		cout << "Hello World\n";
+	}
+}
