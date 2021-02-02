eccc824d79df767d2e7f39df402c8ebb51cf3284
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-02-02 00:24:37 -0600

    files file filelike

--- csci6050/p1/p1.cpp
+++ csci6050/p1/p1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,5 +8,15 @@ int main(int argc, char* argv[]){
 	//base test case
 	if(argc>0 and (string(argv[1]) == "-hw")){
 		cout << "Hello World\n";
+		return 1;
 	}
+	
+	//variables
+	ifstream file;
+	
+	file.open(string(argv[1]));
+	
+	
+	
+	file.close();
 }
