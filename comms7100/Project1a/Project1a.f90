92c538384de258b0c25eb73825a224072d689ea1
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-02-15 12:52:09 -0600

    added some variables

--- comms7100/Project1a/Project1a.f90
+++ comms7100/Project1a/Project1a.f90
@@ -6,11 +6,21 @@ program Project1a
 	!Variables
 	character(100) :: buffer	
 	real :: temp !temperature assumed Kelvin
+	real, allocatable :: guess(:), deltaGuess(:) !paramenters
 	real, allocatable :: dataPoints(:,:) !2d array with the shape [(x1,y1),...,(xm,ym)]
+	real :: M !number of paramenters (real for math reasons) ((not sure if real is necisarry)
+	real :: lambda = 0.001 !starting value of lambda
+	real :: error, newError, deltaError !Can I really declare them this way?
+	real, allocatable :: beta(:), alpha(:,:), alphaMod(:,:), alphaSolve(:) 
+	!a reset variable maybe
+	real :: variance
+	real, allocatable :: standDev(:)
+	
 	
 	!command line argument test
 	if (COMMAND_ARGUMENT_COUNT() >= 1) then
 		call GET_COMMAND_ARGUMENT(1, buffer)
+		stop
 	end if
 	
 	!this is a test function 
