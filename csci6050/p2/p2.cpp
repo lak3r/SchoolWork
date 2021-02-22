95baeee71cce38c95f6978e1a347960697b038b8
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-02-22 00:43:44 -0600

    some thigs

---
+++ csci6050/p2/p2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include <fcntl.h> //this is giving me read/write???
+//#include <stdio.h> //not needed here but is on system64
+//#include <unistd.h> //not needed here but is on system64
+
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+	
+	//base test case
+	if(argc>1 and (string(argv[1]) == "-hw")){
+		cout << "Hello World\n";
+		exit(0);
+	}
+	else if(argc == 3){
+		
+	}
+	else{
+		cout << "Invalid input";
+		exit(0);
+	}
+	
+	
+	return 1;
+}
