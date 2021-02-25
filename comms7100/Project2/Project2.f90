98cd850112d271c4a851ac7fd79252fbf4ad2077
Laurel Koenig <lak3r@users.noreply.github.com>
2021-02-25 10:21:45 -0600

    opned the file

--- comms7100/Project2/Project2.f90
+++ comms7100/Project2/Project2.f90
@@ -12,6 +12,7 @@ program Project2
 	
 	!actually related to the problem at hand
 	
+!initial settup and verifications
 	
 	!get command line input
 	if (command_argument_count() >= 1) then
@@ -25,7 +26,24 @@ program Project2
 		stop
 	end if
 
+	!opening the file
+	inquire(file = buffer, Exist = flag)
+	if (flag) then
+		open(1, file = buffer, status = 'old')
+	else if(flag .eqv. .false.) then
+		print *, 'That file name does not exist. Exiting'
+		stop
+	end if
+	
 
+!reading the data
+	
+	!trivial comment line. Just needs to be printed.
+	read(1, '(A)') buffer
+	print *, buffer
+	
+!clean up
+	close(1)
 
 
 end program Project2
