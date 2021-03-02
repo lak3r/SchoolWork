f5b4c2e9f881f4b9f85ff6912213b94fcbb9b749
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-03-01 20:51:18 -0600

    nothing

--- comms7100/Project2/helpP2.f90
+++ comms7100/Project2/helpP2.f90
@@ -5,7 +5,7 @@ module helpP2
 		! AV: http://computer-programming-forum.com/49-fortran/4075a24f74fcc9ce.ht
 		subroutine lowerCase(word)
 		! convert a word to lower case
-		character (len=*) , intent(in out) :: word
+			character (len=*) , intent(in out) :: word
 			integer                            :: i,ic,nlen
 			nlen = len(word)
 			do i=1,nlen
