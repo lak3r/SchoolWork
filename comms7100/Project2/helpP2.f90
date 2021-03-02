d745a2d7a66a14865d63d1d13a5fc1b4221ca027
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-03-01 20:32:51 -0600

    added a thing

--- comms7100/Project2/helpP2.f90
+++ comms7100/Project2/helpP2.f90
@@ -1,6 +1,16 @@
 module helpP2
 	implicit none
 	
-	
-
+	contains
+			! AV: http://computer-programming-forum.com/49-fortran/4075a24f74fcc9ce.ht
+			subroutine lowerCase(word)
+			! convert a word to lower case
+				character (len=*) , intent(in out) :: word
+				integer                            :: i,ic,nlen
+				nlen = len(word)
+				do i=1,nlen
+				   ic = ichar(word(i:i))
+				   if (ic >= 65 .and. ic < 90) word(i:i) = char(ic+32)
+				end do
+			end subroutine lowerCase
 end module helpP2
