53a72246167874146f87a670421fc1405f971064
Laurel Koenig <35858548+lak3r@users.noreply.github.com>
2021-03-19 05:14:41 -0500

    orbit period hard

--- comms7100/Project3/Project3.f90
+++ comms7100/Project3/Project3.f90
@@ -20,6 +20,7 @@ program Project3
 	real(16), allocatable :: r(:,:), v(:,:)
 	real(16) :: G, pi, mSun, gSun !constants
 	real(16) :: deltT, Tj, mass
+	real(16) :: semimajorAxis, orbitPeriod, eccentricity, meanVelocity
 	integer :: N
 	real(16), dimension(6) :: peri, ap !(rx, ry, rnorm, vx, vy, vnorm)
 	real(16), dimension(2) :: rHalf, vHalf
@@ -56,7 +57,7 @@ program Project3
 	r(2,1) = 0
 	v(1,1) = 0
 	v(2,1) = 58.98
-	deltT = Tj / N !in seconds
+	deltT = Tj / (N - 1) !in seconds
 	print *, "Time step: ", deltT
 	
 	peri(1:2) = r(:,1)
@@ -64,12 +65,13 @@ program Project3
 	peri(4:5) = v(:,1)
 	peri(6) = norm(peri(4:5),2)
 	ap = peri
+	meanVelocity = norm(v(:,1),2)
 	!euler
 	do i=2, N
 		r(:,i) = r(:,i-1) + v(:,i-1) * deltT
 		v(:,i) = v(:,i-1) - ((gSun)/(norm(r(1:2,i-1),2)**3)) * r(:,i-1) * deltT
 		
-		
+		meanVelocity = meanVelocity + norm(v(:,i),2)
 		if(norm(v(:,i),2) > peri(6)) then
 			peri(1:2) = r(:,i)
 			peri(3) = norm(peri(1:2),2)
@@ -83,14 +85,22 @@ program Project3
 		end if
 		
 	end do
-	
+	!Orbit = deltT * i / ( 24 * 60 * 60 )
 	print *, "----------------------------------------------------------------------------"
 	print "(/,A)", "Euler method results"
 	print "(/,A)", "Perihelion of Mercury"
 	print "(2(A,2x,es10.3,2x))", "Distance: ", peri(3), " Speed: ", peri(6)
 	print *, "Aphelion of Mercury"
-	print "(2(A,2x,es10.3,2x))", "Distance: ", ap(3), " Speed: ", ap(6)	
-	
+	print "(2(A,2x,es10.3,2x))", "Distance: ", ap(3), " Speed: ", ap(6)
+	!print "(/,A, f10.3)", "Sidereal orbit period (days): ", orbit
+	semimajorAxis = (abs(peri(3)) + abs(ap(3))) / 2
+	print "(/,A,es10.3)", "Semimajor axis: ", semimajorAxis
+	orbitPeriod = sqrt(semimajorAxis**3 / gSun) / (24 * 60 * 60)
+	print "(/,A,f10.3)", "Sidereal Orbit period: ", orbitPeriod
+	eccentricity = (semimajorAxis - peri(3)) / semimajorAxis
+	print "(/,A,f10.5)", "Orbit eccentricity: ", eccentricity
+	meanVelocity = meanVelocity / N
+	print "(/,A,f10.3)", "Mean orbital velocity: ", meanVelocity
 	
 
 	peri(1:2) = r(:,1)
@@ -98,12 +108,13 @@ program Project3
 	peri(4:5) = v(:,1)
 	peri(6) = norm(peri(4:5),2)
 	ap = peri
+	meanVelocity = norm(v(:,1),2)
 	!cromer
 	do i=2, N
-		
 		v(:,i) = v(:,i-1) - ((gSun)/(norm(r(1:2,i-1),2)**3)) * r(:,i-1) * deltT
 		r(:,i) = r(:,i-1) + v(:,i) * deltT
 		
+		meanVelocity = meanVelocity + norm(v(:,i),2)
 		if(norm(v(:,i),2) > peri(6)) then
 			peri(1:2) = r(:,i)
 			peri(3) = norm(peri(1:2),2)
@@ -124,6 +135,12 @@ program Project3
 	print "(2(A,2x,es10.3,2x))", "Distance: ", peri(3), " Speed: ", peri(6)
 	print *, "Aphelion of Mercury"
 	print "(2(A,2x,es10.3,2x))", "Distance: ", ap(3), " Speed: ", ap(6)	
+	semimajorAxis = (abs(peri(3)) + abs(ap(3))) / 2
+	print "(/,A,es10.3)", "Semimajor axis: ", semimajorAxis
+	eccentricity = (semimajorAxis - peri(3)) / semimajorAxis
+	print "(/,A,f10.5)", "Orbit eccentricity: ", eccentricity
+	meanVelocity = meanVelocity / N
+	print "(/,A,f10.3)", "Mean orbital velocity: ", meanVelocity
 	
 	
 	peri(1:2) = r(:,1)
@@ -131,6 +148,7 @@ program Project3
 	peri(4:5) = v(:,1)
 	peri(6) = norm(peri(4:5),2)
 	ap = peri
+	meanVelocity = norm(v(:,1),2)
 	!Runge-Kutta
 	do i=2, N
 		!take a "trial" step
@@ -141,7 +159,7 @@ program Project3
 		r(:,i) = r(:,i-1) + vHalf * deltT
 		v(:,i) = v(:,i-1) - ((gSun)/(norm(rHalf,2)**3)) * rHalf * deltT
 		
-		
+		meanVelocity = meanVelocity + norm(v(:,i),2)
 		if(norm(v(:,i),2) > peri(6)) then
 			peri(1:2) = r(:,i)
 			peri(3) = norm(peri(1:2),2)
@@ -162,13 +180,29 @@ program Project3
 	print "(2(A,2x,es10.3,2x))", "Distance: ", peri(3), " Speed: ", peri(6)
 	print *, "Aphelion of Mercury"
 	print "(2(A,2x,es10.3,2x))", "Distance: ", ap(3), " Speed: ", ap(6)	
-
+	semimajorAxis = (abs(peri(3)) + abs(ap(3))) / 2
+	print "(/,A,es10.3)", "Semimajor axis: ", semimajorAxis
+	eccentricity = (semimajorAxis - peri(3)) / semimajorAxis
+	print "(/,A,f10.5)", "Orbit eccentricity: ", eccentricity
+	meanVelocity = meanVelocity / N
+	print "(/,A,f10.3)", "Mean orbital velocity: ", meanVelocity
+	
+	
+	
+	!For graphing
+	open(2, file = "mercury.csv")
+	write(2,'(*(G0.6,:,","))') 'X', 'Y'
+	do i=1, N
+		write(2,'(*(G0.6,:,","))') r(1,i), r(2,i)
+	end do
+	print "(/,/,A)", "written."
+	close(2)
 
 !cleanup
 	!timing
 	call cpu_time(finishTime)
 	call system_clock(sysTimeStop)
 	print "(/,/,A, f10.5)", "CPU time: ", finishTime - startTime
-	print "(/,/,A, i10)", "System time: ", sysTimeStop - sysTimeStart
+	print "(A, i10)", "System time: ", sysTimeStop - sysTimeStart
 
 end program Project3
